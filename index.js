e8d647ccb71ee9ad8d6b1adb19890b4542073169
franco77777 <francoaresis@gmail.com>
2022-08-17 12:12:56 -0300

    forcefalse

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js")
 const { getCountries } = require("./src/controllers/controlcountry.js")
 
 const PORT = process.env.PORT || 3001
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, async () => {
     await getCountries()
     console.log("%s listening at 3001") // eslint-disable-line no-console
